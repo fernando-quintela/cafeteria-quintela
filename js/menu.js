f2d0c6a029d98d4c7b4c2571163172acdcb9b581
fernando <fernandoquintela94@gmail.com>
2021-12-10 22:27:16 -0300

    cafeteria terminada

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+let burguerButton = document.getElementById('burguer-button');
+let menu = document.getElementById('menu');
+
+// burguerButton contiene el span con el icono
+// burguerButton.addEventListener('que evento', 'que deseo hacer');
+burguerButton.addEventListener('touchstart', function() {
+  menu.classList.toggle('active');
+});
